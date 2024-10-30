d0f256518d9e16bccb78049cbfe897b5312e8d36
jaxonpowell02 <pow21019@byui.edu>
2024-10-30 16:53:44 -0600

    added repository

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Please enter your name");
+
+        String name;
+
+        name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {name}");
     }
 }
