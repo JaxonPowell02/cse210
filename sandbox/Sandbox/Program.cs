d255d9a0d6aecfadd0a8c09f01f8999b8d13d057
jaxonpowell02 <pow21019@byui.edu>
2024-10-31 09:52:23 -0600

    Added code to test writeline.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,12 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("Please enter your name");
+        // Console.WriteLine("Please enter your name");
 
-        String name;
+        // String name;
 
-        name = Console.ReadLine();
+        // name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is: {name}");
+        // Console.WriteLine($"Your name is: {name}");
+        Console.Write("Input your age: ");
+        string userInput = Console.ReadLine();
+        int age = int.Parse(userInput);
+
+        Console.WriteLine($"Your age is {age}");
+
+        int x = 10;
+
+        if (x > 15){
+            Console.WriteLine("Hey Bob");
+        }
     }
 }
